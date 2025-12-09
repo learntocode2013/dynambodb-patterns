1afca67b8102ae03dd80f1a13e9bc2da1bc2932e
Dibakar Sen <learntocode2013@yahoo.com>
2025-12-10 00:54:12 +0530

    Enable runtime retention for marker annotations

--- src/main/java/com/github/learntocode2013/util/ItemBasedAction.java
+++ src/main/java/com/github/learntocode2013/util/ItemBasedAction.java
@@ -1,5 +1,12 @@
 package com.github.learntocode2013.util;
 
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.METHOD)
 public @interface ItemBasedAction {
 
 }
